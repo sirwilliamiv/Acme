6102580715b7a19a5944bad65a39f9f2c29d0238
sirwilliamiv <wjconnolly2@gmail.com>
2017-01-03 13:35:30 -0600

    added three XHR requests and promises testing loading order

--- acme.js
+++ acme.js
@@ -1,2 +1,72 @@
-// console.log("the javascript")
-// $("body").html("hey")
+// // console.log("the javascript")
+// // $("body").html("hey")
+// Create a simple user interface for your product catalog where a user
+// can select a category from a dropdown.
+// When a category is selected, you must use Promises to read, first,
+// from the
+// categories.json
+// to load that array of objects,
+// then load types.json,
+//  then products.json.
+
+
+
+
+var category1 = new Promise(function(resolve, reject){
+  var xml = new XMLHttpRequest();
+  xml.addEventListener("load", function(){
+    var list = JSON.parse(xml.responseText)
+    resolve(list)
+  })
+  xml.open("GET", "categories.json")
+  xml.send()
+})
+var types2 = new Promise(function(resolve, reject){
+  var xml = new XMLHttpRequest();
+  xml.addEventListener("load", function(){
+    var list = JSON.parse(xml.responseText)
+    resolve(list)
+  })
+ xml.open("GET", "types.json")
+  xml.send()
+})
+
+var products3 = new Promise(function(resolve, reject){
+  var xml = new XMLHttpRequest();
+  xml.addEventListener("load", function(){
+    var list = JSON.parse(xml.responseText)
+    resolve(list)
+  })
+  xml.open("GET", "products.json")
+  xml.send()
+})
+
+
+
+  category1
+    .then(
+  function(val){
+    category1 = val
+    console.log("promise one resolve, ", category1)
+    return types2
+  }).then(
+  function(val){
+    types2 = val
+    console.log("promise two resolve, ", types2)
+    return products3
+  }).then(
+  function(val){
+    products3 = val
+    console.log("promise three resolve, ", products3)
+  })
+
+
+
+
+// function sendToTheDom(passtheValue)
+// // Once all data is loaded, you need to display the products in
+//  a Bootstrap grid.
+//   Each product must display the string name of
+      //  // its product type,
+      //  and product category.
+      //  Not the integer id value.
