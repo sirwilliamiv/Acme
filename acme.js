3ec07a6692e3ff609daa9e83c01e33ed84d5c1af
sirwilliamiv <wjconnolly2@gmail.com>
2017-01-04 22:29:46 -0600

    accessed types category id

--- acme.js
+++ acme.js
@@ -50,8 +50,11 @@ var products3 = new Promise(function(resolve, reject){
 
 
 var theProductName;
- var theTypeIdMatcher;
+var theTypeIdMatcher;
+var theCategoryMatcher;
  // ==========================promises================================
+function fireTheFireworks() {
+
 
   category1
     .then(
@@ -63,22 +66,34 @@ var theProductName;
   function(val){
     types2 = val
     console.log("promise two resolve, ", types2.types)
-  // for(var i = 0; i < types2.types.length; i ++) {
-  // }
+    for (var t in types2.types){
+        console.log( types2.types[t].categoryid)
+
+      // console.log(t)
+      // theCategoryMatcher = types2.types[i];
+      // console.log(theCategoryMatcher)
+    }
     return products3
   }).then(
   function(val){
     products3 = val
     for (var p in products3.products[0]) {
-      // console.log(p)
+
+//saving data
       theProductName = p;
       theTypeIdMatcher = products3.products[0][p].typeid;
-      $('#data').append(`<h1>${theProductName}</h1> <h3>type id: ${theTypeIdMatcher}</h3>`)
+
+      // console.log(theCategoryMatcher)
+// if(theTypeIdMatcher === 0)
+      //send data to DOM
+      $('#data').append(`<div class="fireworks col-md-3">${theProductName}</div> <divclass="fireworks">type id: ${theTypeIdMatcher}</div>`)
 
 }
     console.log("promise three resolve, ", products3)
     // $('#button').append(loadCategories)
   })
+
+}
 
 // how do i associate this thing with that thing
 
@@ -88,81 +103,11 @@ var theProductName;
 
 
 
- // var fireworks = $('#fireworks').click( function(){
- //   console.log($('#fireworks').text() )
- //   loadFireworks +=
- //   if()
- //   $("#data").html(loadFireworks)
- // })
-
- //  var demolition = $('#demolition').click( function(){
- //   console.log($('#demolition').text() )
- //   console.log("load the dom with demolition")
-
- // })
+ $('#fireworks').click( function(){
+        fireTheFireworks()
+ })
 
 
-  // =========================loop to find products=======================================
-
-
-// // var category;
-// // var type;  -=--> types2.types[3].id  to match category
-// // use switch statement
-// // switch(types2.types[3].id){
-// //
-// function letMeMatchTheType(productid) {
-//   switch (productid) {
-//     case 0:
-
-
-// //   case -1:
-// //     console.log('negative 1');
-// //     break;
-//     }
-// //   case 0: // foo is 0 so criteria met here so this block will run
-// //     console.log(0);
-// // //  case 0
-// // //  case1
-// // //  case2
-// // //  case3
-// // //  case4
-// // //  case5
-// // //  case6
-// // //  }
-// // // var product; ---> search products
-// // var usingProducts;
-
-// console.log(types2.types)
-for (var p in types2.types) {
-    // for var k in
-  console.log("please be the products", p)
-  // if (products3[i].)
-  //   use filter to return values and assign values to type
-}
-
-// for(var i = 0; i < products3.length; i++){
-//   if (types[i].)
-//     use filter to return values and assign values to type
-// }
-
-
-
-
-
-
-
-// function sendToTheDom(passtheValue)
-    // loadCategories =
-    // `<div class="card col-md-3">
-    //     <ul class="list-group list-group-flush">
-    //       <li class="list-group-item">line1</li>
-    //       <li class="list-group-item">hey</li>
-    //       <li class="list-group-item">line3</li>
-    //     </ul>
-    // </div>`
-// // Once all data is loaded, you need to display the products in
-//  a Bootstrap grid.
-//   Each product must display the string name of
-      //  // its product type,
-      //  and product category.
-      //  Not the integer id value.
+ $('#demolition').click( function(){
+        fireTheFireworks()
+  })
